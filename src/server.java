9cc3bc4a5a7ef5085477cf2959c543be099399a7
kwadwo485 <57927146+kwadwo485@users.noreply.github.com>
2021-09-24 16:48:15 -0400

    Client server communication

---
+++ src/server.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class server {
+
+    public static void main(String[] args) throws IOException {
+        ServerSocket ss = new ServerSocket(4999);
+        Socket s = ss.accept();
+        System.out.println("Successfully connected to client");
+
+        InputStreamReader in = new InputStreamReader(s.getInputStream());
+        BufferedReader bf = new BufferedReader(in);
+        String str = bf.readLine();
+        System.out.println("Client : " +str);
+
+        //server to client
+        PrintWriter pr = new PrintWriter(s.getOutputStream());
+        pr.println("Server response: ");
+        pr.flush();
+    }
+
+}
