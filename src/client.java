0c1868da0747a91a8e0f22cae2581e985ec4cbe7
kwadwo485 <57927146+kwadwo485@users.noreply.github.com>
2021-09-25 09:10:55 -0400

    Client to server via user input

--- src/client.java
+++ src/client.java
@@ -3,6 +3,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class client {
     public static void main(String[] args) throws IOException {
@@ -10,12 +11,15 @@ public class client {
 
         //client to server
         PrintWriter pr = new PrintWriter(s.getOutputStream());
-        pr.println("Client to server message");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter message to be encrypted: ");
+        String message = sc.nextLine();
+        pr.println(message);
         pr.flush();
 
         InputStreamReader in = new InputStreamReader(s.getInputStream());
         BufferedReader bf = new BufferedReader(in);
         String str = bf.readLine();
-        System.out.println("Server : " +str);
+        System.out.println("Server response: " +str);
     }
 }
