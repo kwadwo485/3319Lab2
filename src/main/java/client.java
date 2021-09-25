d4a4f0b829d172b55cbf0b18bc0e64a4f8e7181b
kwadwo485 <57927146+kwadwo485@users.noreply.github.com>
2021-09-25 10:05:15 -0400

    added maven, dependencies, successful hmac encryption

--- src/main/java/client.java
+++ src/main/java/client.java
@@ -1,17 +1,20 @@
 import javax.crypto.Mac;
-import javax.crypto.SecretKey;
 import javax.crypto.spec.SecretKeySpec;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
-import java.nio.charset.StandardCharsets;
+import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
 import java.util.Scanner;
 
+import static java.util.Base64.*;
+import static org.apache.commons.codec.binary.Base64.encodeBase64String;
+
 public class client {
-    public static void main(String[] args) throws IOException, NoSuchAlgorithmException {
+    public static void main(String[] args) throws IOException, NoSuchAlgorithmException, InvalidKeyException {
         Socket s = new Socket("localhost", 4999);
 
         //client to server
@@ -22,8 +25,11 @@ public class client {
         String message = sc.nextLine();
         Mac sha256 = Mac.getInstance("HmacSHA256");
         SecretKeySpec sks = new SecretKeySpec(secret.getBytes(),"HmacSHA256");
-
-        pr.println(message);
+        sha256.init(sks);
+        
+        String hmacMessage = encodeBase64String(sha256.doFinal(message.getBytes()));
+        
+        pr.println(hmacMessage);
         pr.flush();
 
         InputStreamReader in = new InputStreamReader(s.getInputStream());
