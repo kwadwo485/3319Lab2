13eb5b9a4762c574bcd895f30e5898cf06f86340
kwadwo485 <57927146+kwadwo485@users.noreply.github.com>
2021-09-27 10:12:03 -0400

    sends cipher text and hmac digest to server. server not decrypting message

--- src/main/java/client.java
+++ src/main/java/client.java
@@ -1,41 +1,65 @@
-import javax.crypto.Mac;
+import javax.crypto.*;
 import javax.crypto.spec.SecretKeySpec;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
+import java.io.*;
 import java.net.Socket;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
-import java.util.Base64;
 import java.util.Scanner;
 
-import static java.util.Base64.*;
 import static org.apache.commons.codec.binary.Base64.encodeBase64String;
 
 public class client {
-    public static void main(String[] args) throws IOException, NoSuchAlgorithmException, InvalidKeyException {
+    public static void main(String[] args) throws IOException, NoSuchAlgorithmException, InvalidKeyException,
+            NoSuchPaddingException, IllegalBlockSizeException, BadPaddingException {
         Socket s = new Socket("localhost", 4999);
 
         //client to server
         PrintWriter pr = new PrintWriter(s.getOutputStream());
+        //PrintWriter pr2 = new PrintWriter(s.getOutputStream());
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter message to be encrypted: ");
-        String secret = "admin";
+        String hmacKey = "admin";
         String message = sc.nextLine();
+        byte[] plainText = message.getBytes();
+
+        //UTF
+        OutputStream outputStream = s.getOutputStream();
+        DataOutputStream dataOutputStream = new DataOutputStream(outputStream);
+
         Mac sha256 = Mac.getInstance("HmacSHA256");
-        SecretKeySpec sks = new SecretKeySpec(secret.getBytes(),"HmacSHA256");
+        Cipher cipherObject = Cipher.getInstance("DES");
+
+        SecretKeySpec sks = new SecretKeySpec(hmacKey.getBytes(),"HmacSHA256");
+        KeyGenerator kg = KeyGenerator.getInstance("DES");
         sha256.init(sks);
-        
-        String hmacMessage = encodeBase64String(sha256.doFinal(message.getBytes()));
-        
-        pr.println(hmacMessage);
+        SecretKey sk = kg.generateKey();
+
+        //using UTF for hmac digest
+        String hmacDigest = encodeBase64String(sha256.doFinal(message.getBytes()));
+        dataOutputStream.writeUTF(hmacDigest);
+        dataOutputStream.flush(); // send the message
+
+        cipherObject.init(Cipher.ENCRYPT_MODE, sk);
+        byte[] encodedMessage = cipherObject.doFinal(plainText);
+
+        //for human observer
+        System.out.println("Message: " +message);
+        System.out.println("HMAC Key: " +hmacKey);
+        System.out.println("HMAC digest: " +hmacDigest);
+        System.out.println("Cipher text:" + new String(encodedMessage));
+
+        //send cipher text to server
+        pr.println(new String(encodedMessage) +"\n");
         pr.flush();
 
+        //get decoded text from server
         InputStreamReader in = new InputStreamReader(s.getInputStream());
         BufferedReader bf = new BufferedReader(in);
         String str = bf.readLine();
         //response from server
-        System.out.println("Response: " +str);
+        System.out.println("Server: Decoded message - " +str);
+
+        //closing UTF
+        dataOutputStream.close(); // close the output stream when we're done.
     }
 }
--- src/main/java/server.java
+++ src/main/java/server.java
@@ -1,26 +1,46 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
+import javax.crypto.*;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
 
 public class server {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException,
+            IllegalBlockSizeException, BadPaddingException {
         ServerSocket ss = new ServerSocket(4999);
         Socket s = ss.accept();
-        System.out.println("Successfully connected to client");
+        System.out.println("Successfully connected to client...\n\n");
 
+        //from client
         InputStreamReader in = new InputStreamReader(s.getInputStream());
         BufferedReader bf = new BufferedReader(in);
-        String str = bf.readLine();
-        System.out.println("Client : " +str);
 
-        //server to client
+        //UTF (server client communication 2)
+        InputStream inputStream = s.getInputStream();
+        DataInputStream dataInputStream = new DataInputStream(inputStream);
+        String readHmacDigest = dataInputStream.readUTF();
+        System.out.println("Received HMAC digest: " +readHmacDigest);
+
+        String str = bf.readLine();
+
+        //For human observer
+        System.out.println("Client: Encrypted plain text - " +str);
+
+       Cipher cipherObject = Cipher.getInstance("DES");
+       KeyGenerator kg = KeyGenerator.getInstance("DES");
+       SecretKey sk = kg.generateKey();
+       cipherObject.init(Cipher.DECRYPT_MODE, sk);
+
+        String str2 = "";
+        byte[] receivedText = str2.getBytes();
+        byte[] decodedMessage = cipherObject.doFinal(receivedText);//main problem: can't decode str
+        System.out.println("Decoded message: " +new String(decodedMessage));
+
+        //server back to client
         PrintWriter pr = new PrintWriter(s.getOutputStream());
-        //pr.println("Server says - " +str.replace(str,"Don't talk to me"));
-        pr.println("Server says - " +str);
+        pr.println("Server says - " +new String (decodedMessage));
         pr.flush();
     }
 
